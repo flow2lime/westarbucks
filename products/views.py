7908df4288e68fe711b020fead090846129d24b0
Hope Kim <kzang2028@gmail.com>
2021-08-17 16:42:57 +0900

    Add: created urls.py in products & added Create views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+import json
+from products.models import Menu, Category, Product
+
+class ProductsView(View):
+    def post(self,.request):
+        data = json.loads(request.body)
+        menu = Menu.objects.create(name=data['menu'])
+        category = Category.objects.create(
+            name = data['category'],
+            menu = menu
+            )
+        Product.objects.create(
+            name     = data['product'],
+            category = category,
+            menu     = menu
+        )
+        return HttpResponse({'MESSAGE': 'CREATED'}, status=201)
